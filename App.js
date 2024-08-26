fd93f00075b2c391d11408eca136bdf9145c5fac
Christian Kirschberg <ckirschberg@gmail.com>
2024-08-26 12:13:36 +0200

    tabs

--- App.js
+++ App.js
@@ -1,30 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import CatScreen from './screens/CatScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SettingsScreen from './screens/SettingsScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+function StackNavigator() {
+  return (
+     <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Cat" component={CatScreen} />
+      </Stack.Navigator>
+  )
+}
 
 export default function App() {
-
-  const Stack = createNativeStackNavigator();
 
   return (
     // Når vi bruger navigation, skal koden 'wrappes' i en NavigationContainer
     <NavigationContainer> 
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-      <Stack.Navigator>
-        {/* name er det navn vi bruger senere når vi vil navigere til en bestemt side.
-        component er navnet på componenten */}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Cat" component={CatScreen} />
-      </Stack.Navigator>
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'information-circle'
+                : 'information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'list' : 'list-outline';
+            }
 
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={StackNavigator} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+
+
+      {/* name er det navn vi bruger senere når vi vil navigere til en bestemt side.
+          component er navnet på componenten */}
+        
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
+  //container svarer til en css-klasse
   container: {
     flex: 1,
     backgroundColor: '#fff',
