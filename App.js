36c9d4bff5abbbe0f822e8bd829a6c05b7168644
Christian Kirschberg <ckirschberg@gmail.com>
2024-08-28 11:00:14 +0200

    .

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import GPSComponent from './components/GPSComponent';
 import GPSandMapComponent from './components/GPSandMapComponent';
 import GesturesComponent from './components/GesturesComponent';
 import CameraComponent from './components/CameraComponent';
-import HomeScreen from './screens/HomeScreen';
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -22,10 +22,10 @@ function StackNavigator() {
      <Stack.Navigator>
 
         {/* Vis HomeScreen for at se kamera eksemplet */}
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/> */}
 
         {/* Vis GPSandMapComponent for at se maps eksemplet */}
-        {/* <Stack.Screen name="Home" component={GPSandMapComponent} options={{ headerShown: false }}/> */}
+        <Stack.Screen name="Home" component={GPSandMapComponent} options={{ headerShown: false }}/>
 
         {/* Vis GesturesComponent for at se gestures eksemplet */}
         {/* <Stack.Screen name="Home" component={GesturesComponent} options={{ headerShown: false }}/> */}
